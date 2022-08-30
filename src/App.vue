1a22d3c566005f88198fa20b83d43e348c2cfa91
Ariel <arielfontes15@hotmail.com>
2022-08-30 00:17:59 -0300

    estilizando minha pagina de quizz

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <div></div>
+  <div class="menu">
+    <h1>Mamãe no céu tem pão?</h1>
+    <div>
+      <input type="radio" name="options" value="True">
+      <label>True</label><br>
+      <input type="radio" name="options" value="False">
+      <label>False</label><br>
+    </div>
+    <button class="btnEnviar">Enviar</button>
+  </div>
 </template>
 
 <script>
@@ -9,6 +18,8 @@ export default {
   components: {
   }
 }
+
+//https://opentdb.com/api.php?amount=10&category=18
 </script>
 
 <style lang="scss">
@@ -17,7 +28,30 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #0b0d0f;
+  margin: 60px auto;
+  max-width: 960px;
+
+  .menu {
+    background: linear-gradient(#5896d3, rgb(182,153, 153));
+    border-radius: 4px;
+    padding-top: 20px;
+    
+    .btnEnviar{
+      box-shadow: rgba(0, 0, 0, 0.5);
+      border: none;
+      width: 150px;
+      height: 30px;
+      border-radius: 4px;
+      cursor: pointer;
+      margin: 20px auto;
+      background-color: rgb(212, 201, 218);
+    }
+  }
+  
+  input[type=radio] {
+    display: inline-block;
+    margin: 12px 4px;
+  }
 }
 </style>
